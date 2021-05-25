6667d063147c4cabb432957131090110416a0561
Rakin Gilani <rakin@gilani.com>
2021-05-25 14:53:06 +0100

    Rakin final version of the regressor, now Regressor takes arguments of the cross validator and takes optional arguments that changes the paramaters of the cross validator

--- regression.py
+++ regression.py
@@ -10,9 +10,10 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn import svm
 
 # cv = tscv, kf, etc
-def load(fname: str, training_ratio: float, cv: str, num_splits: int = 5) -> tuple:
+def load(fname: str, training_ratio: float, cv: str) -> tuple:
     # basic spliter for now
     """Loads 2 column data from a CSV file with 2 columns, x and y"""
+
     # read data
     data = pd.read_csv(fname)
     y_column = 'stroke'
@@ -23,27 +24,14 @@ def load(fname: str, training_ratio: float, cv: str, num_splits: int = 5) -> tup
     X_columns.remove(y_column)
     X = data[X_columns].to_numpy()
     y = data[y_column].to_numpy()
+)
 
-    #  tscv = TimeSeriesSplit()
-    #  print(tscv)
-
-    # we want to override n_splits
-    tscv = TimeSeriesSplit(gap=0, max_train_size=None, n_splits=num_splits, test_size=None)
-    kf = KFold(n_splits=num_splits, random_state=None, shuffle=False)
-
-    # how are we going to cross-validate them
-
-    # common for all splitting techniques:
+    # splitting calling the scikit split method of each cross validator
     for train_index, test_index in cv.split(X):
         print("TRAIN:", train_index, "TEST:", test_index)
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
 
-    # do split
-    #X_train = X[:n_rows]
-    #X_test = X[n_rows:]
-    #y_train = y[:n_rows]
-    #y_test = y[n_rows:]
 
     # reshape x to ensure it is 2D
     # fixme: already 2D?
@@ -55,12 +43,18 @@ def load(fname: str, training_ratio: float, cv: str, num_splits: int = 5) -> tup
 
 class Regressor:
     """Container for analysing different metrics for a single regression class"""
-    def __init__(self, cls, fname, training_ratio, **kwargs):
+    def __init__(self, cls, fname: str, cv, cls_kwargs: dict, cv_kwargs: dict ):
         # construct regressor object
-        self.regressor = cls(**kwargs)
+
+        #the regressor and cv do not take the same arguments however
+
+
+        self.regressor = cls(**cls_kwargs)
+        self.cv = cv(**cv_kwargs)
 
         # use load function
-        self.X, self.y, self.X_train, self.y_train, self.X_test, self.y_test = load(fname, training_ratio)
+        # where cv is a splitting class i.e kFold()
+        self.X, self.y, self.X_train, self.y_train, self.X_test, self.y_test = load(fname, training_ratio, self.cv)
 
         # fit data
         self.regressor.fit(self.X_train, self.y_train)
